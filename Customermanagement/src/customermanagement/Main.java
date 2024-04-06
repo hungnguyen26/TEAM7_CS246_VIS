5748f81449198963093cb6c20786ba700942a637
ngocson2811 <161575159+ngocson2811@users.noreply.github.com>
2024-04-06 10:06:43 +0700

    them sua xoa

---
+++ Customermanagement/src/customermanagement/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customermanagement;
+
+/**
+ *
+ * @author WIN-PRO
+ */
+public class Main {
+    public static void main(String[] args) {
+        JFCustomermanagement jfc = new JFCustomermanagement();
+        jfc.show();
+    }
+}
